d4d8d411aaa2f1f9a012c5324dee79a57512e750
Aldemir Humberto Soares Neto <56364675+aldemirneto@users.noreply.github.com>
2022-06-09 10:37:29 -0300

    Delete configfile

--- delete/config.py
+++
@@ -1,8 +0,0 @@
-def config():
-    argumentos = {
-        "user":"remoto",
-        "password":"Ag0r4V4i!",
-        "path":"192.168.0.16:3306",
-        "bd":"padrao"
-    }
-    return argumentos
